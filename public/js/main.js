448570b764df6e313c59832e8bd260ada9958f24
Chris0925-coder <christianguardia@outlook.es>
2024-02-19 22:02:40 -0500

    003

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,18 @@
 
-const navBoton = document.querySelector(".navbar-toggler");
-const navLink = document.querySelector(".off");
-const close = document.querySelector(".btn-close");
+// const navBoton = document.querySelector(".navbar-toggler");
+// const navLink = document.querySelector(".off");
+const close = document.querySelector(".close");
+const schedule = document.querySelector(".content-schedule");
+const showScheduleF1 = document.querySelector(".view-scheduleF1");
 
+// navBoton.addEventListener('click', () => {
+//     navLink.classList.toggle("hidden-nav");
+// });
 
-navBoton.addEventListener('click', () => {
-    navLink.classList.toggle("hidden-nav");
+showScheduleF1.addEventListener('click', () => {
+    schedule.style.display = "grid";
 });
 
 close.addEventListener('click', () => {
-    navLink.classList.toggle("hidden-nav");
+    schedule.style.display = "none";
 });
