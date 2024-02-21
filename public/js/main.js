b44aea5140589535295a9abaf82a28f7432e1070
Chris0925-coder <christianguardia@outlook.es>
2024-02-21 11:11:44 -0500

    21022024

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,10 @@ const navBoton = document.querySelector(".navbar-toggler");
 const navLink = document.querySelector(".navbar-collapse");
 const close = document.querySelector(".close");
 const closeA = document.querySelector(".closeA");
+const closeB = document.querySelector(".closeB");
 const schedule = document.querySelector(".content-schedule");
+const res = document.querySelector(".res-one");
+const resultA = document.querySelector(".result-one");
 const showScheduleF1 = document.querySelector(".view-scheduleF1");
 
 navBoton.addEventListener('click', () => {
@@ -14,8 +17,14 @@ showScheduleF1.addEventListener('click', () => {
     schedule.style.display = "grid";
 });
 
+resultA.addEventListener('click', () => {
+    res.style.display = "flex";
+});
+
+
 close.addEventListener('click', () => {
     schedule.style.display = "none";
+    // res.style.display = "none";
     // schedule.classList.toggle("navbar-nav_visible");
     // navLink.style.display = "none";
     // navLink.classList.toggle("navbar-nav_visible");
@@ -23,6 +32,13 @@ close.addEventListener('click', () => {
 
 closeA.addEventListener('click', () => {
     navLink.classList.toggle("navbar-nav_visible");
+    // navLink.style.transform = "translateX(10px)";
+    // navLink.style.transition = "all .3s ease-in-out";
+});
+
+closeB.addEventListener('click', () => {
+    // navLink.classList.toggle("navbar-nav_visible");
+    res.style.display = "none";
     // navLink.style.transform = "translateX(10px)";
     // navLink.style.transition = "all .3s ease-in-out";
 });
