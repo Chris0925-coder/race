792c1dd86ee908765c7b8fb5670a880d395c288e
Chris0925-coder <christianguardia@outlook.es>
2024-03-10 09:06:53 -0500

    031024B

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ const closeA = document.querySelector(".closeA");
 const closeB = document.querySelectorAll(".closeB");
 const schedule = document.querySelector(".content-schedule");
 const res = document.querySelector(".res-one");
-const images = document.querySelector(".saudi-img");
+// const images = document.querySelector(".saudi-img");
 const resultA = document.querySelector(".result-one");
 // const resultB = document.querySelector(".news__content");
 const showScheduleF1 = document.querySelector(".view-scheduleF1");
@@ -59,4 +59,4 @@ function cc(a, b) {
 };
 
 cc(closeB[0], res);
-cc(closeB[1], images);
+// cc(closeB[1], images);
