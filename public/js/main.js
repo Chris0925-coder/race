3f33d5e17d02eed12c14ddb8a8dfec9f325624b5
Chris0925-coder <christianguardia@outlook.es>
2024-04-10 21:24:28 -0500

    0004

--- public/js/main.js
+++ public/js/main.js
@@ -109,27 +109,12 @@ function statusChangeCallback(response) {  // Called with the results from FB.ge
     }
   }
 
-
   function checkLoginState() {               // Called when a person is finished with the Login Button.
     FB.getLoginStatus(function(response) {   // See the onlogin handler
       statusChangeCallback(response);
     });
   }
 
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '985895733055530',
-      cookie     : true,                     // Enable cookies to allow the server to access the session.
-      xfbml      : true,                     // Parse social plugins on this webpage.
-      version    : 'v19.0'           // Use this Graph API version for this call.
-    });
-
-
-    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-      statusChangeCallback(response);        // Returns the login status.
-    });
-  };
  
   function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
     console.log('Welcome!  Fetching your information.... ');
