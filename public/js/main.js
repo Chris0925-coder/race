da4c98638314d4e86be2975eb7adf31d3896e9f5
Chris0925-coder <christianguardia@outlook.es>
2024-04-10 20:09:19 -0500

    04102024

--- public/js/main.js
+++ public/js/main.js
@@ -62,6 +62,113 @@ cc(closeB[0], res);
 // cc(closeB[1], images);
 
 
+$(function () {
+    let appId = '985895733055530';
+    let scopes = 'name, email, user_friends, user_online_presence';
+    let btn_login = '<a href="#" id="login" class="fb_btn"><button type="submit"></button></a>';
+    let divSession = '<div id="fb-session">'+
+                    '<strong></strong>'+
+                    '<img>'+
+                    '<a href="#" id="logout" class="btn_logout">Cerrar sesión</a>'+
+                    '</div>';
+
+     window.fbAsyncInit = function() {
+        FB.init({
+          appId      : appId,
+          status     : true,
+          cookie     : true,
+          xfbml      : true,
+          version    : '{v19.0}'
+        });
+          
+        // FB.AppEvents.logPageView();
+
+        FB.getLoginStatus(function(response) {
+        statusChangeCallback(response, function() {
+            // callback(data);
+        });
+    });
+          
+      };
+
+      function statusChangeCallback(response, callback) {
+        // console.log('statusChangeCallback');
+        console.log(response);
+        if (response.status === 'connected') {
+          // testAPI();
+          getFbData();  
+        } else {
+            callback(false);                         
+          // document.getElementById('status').innerHTML = 'Please log ' +
+            // 'into this webpage.';
+        }
+      // }
+
+    // FB.login(function(response) {
+    //       if (response.authResponse) {
+    //            console.log('Welcome!  Fetching your information.... ');
+    //            FB.api('/me', function(response) {
+    //             $('#login').after(divSession);
+    //             $('#login').remove();
+    //             $('#facebook-session strong').text("Bienvenido: "+ response.name);
+    //             $('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=large');
+    //           console.log('Successful login for: ' + response.name);
+    //           // document.getElementById('status').innerHTML =
+                // 'Thanks for logging in, ' + response.name + '!';
+            // })
+               // FB.api('/me', {fields: 'name, email'}, function(response) {
+               //     document.getElementById("profile").innerHTML = "Good to see you, " + response.name + ". i see your email address is " + response.email
+               // });
+    //       } else { 
+    //            console.log('User cancelled login or did not fully authorize.'); 
+           }
+    // });
+
+    function checkLoginState(response,callback) {             
+        FB.getLoginStatus(function(response) {   
+          statusChangeCallback(response, function(data) {
+            callback(data);
+          });
+
+        });
+    };
+
+    function getFbData() {
+        // if (response.authResponse) {
+        // console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+            $('#login').after(divSession);
+            $('#login').remove();
+            $('#facebook-session strong').text("Bienvenido: "+ response.name);
+            $('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=large');
+          console.log('Successful login for: ' + response.name);
+          document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+        })
+    } 
+        // else {
+        //     console.log('User cancelled login or did not fully authorize.');
+        // }
+    // }
+
+    function fbLogin() {
+        checkLoginState((response) => {
+            if (!response){
+                FB.login(function(response) {
+                    if (response.status === 'connected')
+                        getFbData();
+                    }, {scopes: scopes});
+            }
+        });
+    }
+
+    $(document).on('click', '#login', function(e) {
+        e.preventDefault();
+        fbLogin();
+    })
+});
+
+
 const date = document.querySelector('.date');
 
 date.append(new Date ().getFullYear ());
