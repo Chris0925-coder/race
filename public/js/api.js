1eed0f1a4491f47eda4fe9efe4b3b625825a810a
Chris0925-coder <christianguardia@outlook.es>
2024-04-25 20:17:09 -0500

    005

--- public/js/api.js
+++ public/js/api.js
@@ -1,10 +1,10 @@
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery__insta');
 const feed = document.querySelector('.gallery__container');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 
 const token = 'IGQWRPS2FIYWx6MUtxdzk5RkpVNDctYUwzUEE2bjJwalNiaWFUcm5wUlFUNjEwbG43X3NoRWgwWHQ1aWM1R3REZAWpsZAk8yY25XTVd3VXczblJqMXhrcC10S1k1cFJ6RjlOQS1mTDhfODNNVllXUmhVNFhQLUhfWEUZD';
-const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=80&access_token=${token}`; 
+const url = `https://graph.instagram.com/me/media?fields=username,thumbnail_url,media_url,caption,timestamp,permalink&limit=80&access_token=${token}`; 
 
 fetch(url)
 .then(res => res.json())
@@ -12,15 +12,19 @@ fetch(url)
 
 function CrearHtml(data) {
     for (const img of data) {
-        console.log(img);
-        gallery.innerHTML += `<div class="image overflow">
-                            <img loading="lazy" src="${img.media_url}" alt="#">
-                            <div class="opacity__hover">
-                                <a href="${img.permalink}" class="caption">
-                                    <p>${img.caption.slice(0,100)}</p>
-                                </a>
-                            </div>
-                        </div>`;
+        console.log(data);
+        // let caption = img.caption;
+        // let cap = caption;
+        if (img.caption === undefined) {img.caption = ''}
+        gallery.innerHTML += `
+                            <div class="image overflow">
+                                <img loading="lazy" src="${img.media_url}" alt="#">
+                                <div class="opacity__hover">
+                                    <a href="${img.permalink}" class="caption">
+                                        <p>${img.caption}<br>${img.timestamp}</p>
+                                    </a>
+                                </div>
+                            </div>`;
     }
 };
 
